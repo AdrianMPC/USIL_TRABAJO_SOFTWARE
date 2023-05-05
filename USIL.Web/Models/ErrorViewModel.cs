03828df31a6d0fb0359082332900d5fcf57f0da7
AdrianMPC <83367059+AdrianMPC@users.noreply.github.com>
2023-05-05 17:38:27 -0500

    DATA EXTRA NO NECESARIA
    Algo que quedó del proyecto que sobrescribí

--- USIL.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace USIL.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
